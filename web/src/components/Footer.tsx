95c193b9147f23dd58d4b2f05e22037c617f8f20
jpkam <jpkamdem@proton.me>
2024-12-01 16:36:08 +0100

    initialisation web, Dockerfile, router, tailwindcss

---
+++ web/src/components/Footer.tsx
@@ -0,0 +1,9 @@
+export default function Footer() {
+  return (
+    <>
+      <footer className="p-2 absolute bottom-0 w-screen text-center text-white bg-slate-900">
+        footer works!
+      </footer>
+    </>
+  );
+}
---
+++ web/src/components/Header.tsx
@@ -0,0 +1,9 @@
+export default function Header() {
+  return (
+    <>
+      <header className="p-2 text-center text-white bg-orange-700">
+        header works!
+      </header>
+    </>
+  );
+}
