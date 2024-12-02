34399a6166cc9203328a0a4950e020d78a931357
jpkam <jpkamdem@proton.me>
2024-12-02 16:58:04 +0100

    ArticleController & AdminController fini

--- api/src/Controller/AdminController.php
+++ api/src/Controller/AdminController.php
@@ -2,15 +2,110 @@
 
 namespace App\Controller;
 
+use App\Entity\Admin;
+use App\Repository\AdminRepository;
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 class AdminController extends AbstractController
 {
-    #[Route('/api/admin', name: 'app_get_all_admin')]
-    public function index(): JsonResponse
+    #[Route('/api/admin', name: 'api_get_all_admin', methods: ['GET'])]
+    public function getAllAdmins(AdminRepository $adminRepository): JsonResponse
     {
-        return new JsonResponse(['message' => 'Bienvenue l\'API des admins']);
+        $admins = $adminRepository->findAll();
+
+        $data = [];
+        foreach ($admins as $admin) {
+            $data[] = [
+                'id' => $admin->getId(),
+                'username' => $admin->getUsername(),
+                'password' => $admin->getPassword(),
+            ];
+        };
+
+        return new JsonResponse($data, JsonResponse::HTTP_OK);
+    }
+    #[Route('/api/admin/{id}', name: 'api_get_one_admin', methods: ['GET'])]
+    public function getOneAdmin(AdminRepository $adminRepository, int $id): JsonResponse
+    {
+        $admin = $adminRepository->find($id);
+
+        if (!$admin) {
+            return new JsonResponse(['status' => 'Admin non trouvé'], Response::HTTP_NOT_FOUND);
+        }
+
+        $data = [
+            'id' => $admin->getId(),
+            'username' => $admin->getUsername(),
+            'password' => $admin->getPassword(),
+        ];
+
+        return new JsonResponse($data, JsonResponse::HTTP_OK);
+    }
+    #[Route('/api/admin', name: 'api_post_admin', methods: ['POST'])]
+    public function createAdmin(Request $request, EntityManagerInterface $entityManager): JsonResponse
+    {
+        $data = json_decode($request->getContent(), true);
+        $username = $data['username'];
+        $password = $data['password'];
+
+        $admin = new Admin();
+        $admin->setUsername($username);
+        $admin->setPassword($password);
+
+        $entityManager->persist($admin);
+        $entityManager->flush();
+
+        return new JsonResponse(['status' => 'Admin créé !'], Response::HTTP_CREATED);
+    }
+    #[Route('/api/admin/{id}', name: 'api_put_admin', methods: ['PUT'])]
+    public function updateAdmin(int $id, Request $request, EntityManagerInterface $entityManager, AdminRepository $adminRepository): JsonResponse
+    {
+        $admin = $adminRepository->find($id);
+
+        if (!$admin) {
+            return new JsonResponse(['status' => 'Admin non trouvé'], Response::HTTP_NOT_FOUND);
+        }
+
+        $data = json_decode($request->getContent(), true);
+        $admin->setUsername($data['username']);
+        $admin->setPassword($data['password']);
+
+        $entityManager->persist($admin);
+        $entityManager->flush();
+
+        return new JsonResponse(['status' => 'Admin modifié !'], Response::HTTP_OK);
+    }
+    #[Route('/api/admin/{id}', name: 'api_delete_one_admin', methods: ['DELETE'])]
+    public function deleteOneAdmin(int $id, EntityManagerInterface $entityManager, AdminRepository $adminRepository): JsonResponse
+    {
+        $admin = $adminRepository->find($id);
+
+        if (!$admin) {
+            return new JsonResponse(['status' => 'Admin non trouvé'], Response::HTTP_NOT_FOUND);
+        }
+
+        $entityManager->remove($admin);
+        $entityManager->flush();
+
+        return new JsonResponse(['status' => 'Admin supprimé !'], Response::HTTP_OK);
+    }
+    #[Route('/api/admin', name: 'api_delete_all_admin', methods: ['DELETE'])]
+    public function deleteAdmins(EntityManagerInterface $entityManager, AdminRepository $adminRepository): JsonResponse
+    {
+        $admins = $adminRepository->findAll();
+
+        foreach ($admins as $admin) {
+            $entityManager->remove($admin);
+        }
+
+        $entityManager->flush();
+
+        return new JsonResponse(['status' => 'Tous les admins ont été supprimés !'], Response::HTTP_OK);
     }
 }
--- api/src/Controller/ArticleController.php
+++ api/src/Controller/ArticleController.php
@@ -2,15 +2,109 @@
 
 namespace App\Controller;
 
+use App\Entity\Article;
+use App\Repository\ArticleRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 class ArticleController extends AbstractController
 {
     #[Route('/api/article', name: 'api_get_all_article', methods: ['GET'])]
-    public function index(): JsonResponse
+    public function getAllArticles(ArticleRepository $articleRepository): JsonResponse
     {
-        return new JsonResponse(['message' => 'Bienvenue dans l\'API des articles']);
+        $articles = $articleRepository->findAll();
+
+        $data = [];
+        foreach ($articles as $article) {
+            $data[] = [
+                'id' => $article->getId(),
+                'title' => $article->getTitle(),
+                'body' => $article->getBody(),
+            ];
+        };
+
+        return new JsonResponse($data, JsonResponse::HTTP_OK);
+    }
+    #[Route('/api/article/{id}', name: 'api_get_one_article', methods: ['GET'])]
+    public function getOneArticle(int $id, ArticleRepository $articleRepository): JsonResponse
+    {
+        $article = $articleRepository->find($id);
+
+        if (!$article) {
+            return new JsonResponse(['status' => 'Article non trouvé'], Response::HTTP_NOT_FOUND);
+        }
+
+        $data = [
+            'id' => $article->getId(),
+            'title' => $article->getTitle(),
+            'body' => $article->getBody(),
+        ];
+
+        return new JsonResponse($data, JsonResponse::HTTP_OK);
+    }
+    #[Route('/api/article', name: 'api_post_article', methods: ['POST'])]
+    public function createArticle(Request $request, EntityManagerInterface $entityManager): JsonResponse
+    {
+        $data = json_decode($request->getContent(), true);
+        $title = $data['title'];
+        $body = $data['body'];
+
+        $article = new Article();
+        $article->setTitle($title);
+        $article->setBody($body);
+
+        $entityManager->persist($article);
+        $entityManager->flush();
+
+        return new JsonResponse(['status' => 'Article créé !'], Response::HTTP_CREATED);
+    }
+    #[Route('/api/article/{id}', name: 'api_put_article', methods: ['PUT'])]
+    public function updateArticle(int $id, Request $request, EntityManagerInterface $entityManager, ArticleRepository $articleRepository): JsonResponse
+    {
+        $article = $articleRepository->find($id);
+
+        if (!$article) {
+            return new JsonResponse(['status' => 'Article non trouvé'], Response::HTTP_NOT_FOUND);
+        }
+
+        $data = json_decode($request->getContent(), true);
+        $article->setTitle($data['title']);
+        $article->setBody($data['body']);
+
+        $entityManager->persist($article);
+        $entityManager->flush();
+
+        return new JsonResponse(['status' => 'Article modifié !'], Response::HTTP_OK);
+    }
+    #[Route('/api/article/{id}', name: 'api_delete_article', methods: ['DELETE'])]
+    public function deleteOneArticle(int $id, EntityManagerInterface $entityManager, ArticleRepository $articleRepository): JsonResponse
+    {
+        $article = $articleRepository->find($id);
+
+        if (!$article) {
+            return new JsonResponse(['status' => 'Article non trouvé'], Response::HTTP_NOT_FOUND);
+        }
+
+        $entityManager->remove($article);
+        $entityManager->flush();
+
+        return new JsonResponse(['status' => 'Article supprimé !'], Response::HTTP_OK);
+    }
+    #[Route('/api/article', name: 'api_delete_all_article', methods: ['DELETE'])]
+    public function deleteAllArticles(EntityManagerInterface $entityManager, ArticleRepository $articleRepository): JsonResponse
+    {
+        $articles = $articleRepository->findAll();
+
+        foreach ($articles as $article) {
+            $entityManager->remove($article);
+        }
+
+        $entityManager->flush();
+
+        return new JsonResponse(['status' => 'Tous les articles ont été supprimés !'], Response::HTTP_OK);
     }
 }
