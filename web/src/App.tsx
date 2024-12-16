60bf708b310a24088eabfaffcae81e6aa2272d33
jpkam <jpkamdem@proton.me>
2024-12-16 16:24:48 +0100

    mise à jour web

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,13 +1,74 @@
-import { Outlet } from "react-router-dom";
-import Header from "./components/Header";
-import Footer from "./components/Footer";
+import { useState } from 'react'
+import './App.css'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-export default function App() {
-  return (
+import Header from './assets/components/Header';
+import Home from './assets/pages/Home';
+import Articles from './assets/pages/Articles';
+import Footer from './assets/components/Footer';
+
+
+const App:React.FC = () => {
+  return(
     <>
-      <Header />
-      <Outlet />
-      <Footer />
+      <section>
+      <Router>
+      <Routes>
+
+        <Route
+          path="/"
+          element={
+            <>
+              <Header title="ACCUEIL" />
+              <div><Home/></div>
+              <Footer/>
+            </>
+          }
+        />
+        
+        <Route 
+          path="/articles" element={<Articles />} />
+        <Route
+          path="/saison"
+          element={
+            <>
+              <Header title="SAISON" />
+              <div>Détails de la saison</div>
+            </>
+          }
+        />
+        <Route
+          path="/equipe"
+          element={
+            <>
+              <Header title="EQUIPE" />
+              <div>Présentation de l'équipe</div>
+            </>
+          }
+        />
+        <Route
+          path="/contact"
+          element={
+            <>
+              <Header title="CONTACT" />
+              <div>Contactez-nous</div>
+            </>
+          }
+        />
+        <Route
+          path="/compte"
+          element={
+            <>
+              <Header title="COMPTE" />
+              <div>Votre compte</div>
+            </>
+          }
+        />
+      </Routes>
+    </Router>
+
+      </section>
+
     </>
   );
 }
--- web/src/pages/Saison.tsx
+++ web/src/pages/Saison.tsx
@@ -1,7 +1,27 @@
 export default function Saison() {
   return (
     <>
-      <p>saison works!</p>
+      <div className="mt-8">
+        <div className="text-center text-2xl text-blue-900 ">
+          SAISON ACTUELLE
+        </div>
+
+        <div className="flex justify-center ">
+          <div className=" text-center relative  bg-white border-2 border-slate-100 rounded-t-xl  w-4/6 py-2  ">
+            <div className="absolute  left-10">Terminé</div>
+            <div className="">Match Amical - Aujourd'hui</div>
+          </div>
+        </div>
+        <div className="flex justify-center">
+          <div className="bg-slate-400 w-4/6 ">
+            <div className="flex justify-center relative">
+              <div className="absolute left-0">salut</div>
+              <div>score</div>
+              <div className="absolute right-0">salut</div>
+            </div>
+          </div>
+        </div>
+      </div>{" "}
     </>
   );
 }
